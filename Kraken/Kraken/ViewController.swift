fdd0c8df83af906820536ccff9d405b1bb670f9e
Xavier Mawet <xavier@nakim.be>
2019-06-23 00:37:26 +0200

    Added initial Single View App

---
+++ Kraken/Kraken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kraken
+//
+//  Created by Xavier Mawet on 23/06/2019.
+//  Copyright © 2019 Nakim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
